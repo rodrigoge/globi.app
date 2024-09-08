a07a5236bda5bd291a24647cfec1475e21afa6fc
Rodrigo Gouveia <rgestevao@gmail.com>
2024-09-08 09:37:03 -0300

    feat: adding exception handler with null or empty fields in request

--- api/glycemiaservice/src/main/kotlin/com/globiapp/glycemiaservice/exceptions/ExceptionControllerAdvice.kt
+++ api/glycemiaservice/src/main/kotlin/com/globiapp/glycemiaservice/exceptions/ExceptionControllerAdvice.kt
@@ -1,8 +1,11 @@
 package com.globiapp.glycemiaservice.exceptions
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import java.time.LocalDateTime
 
 @ControllerAdvice
 class ExceptionControllerAdvice {
@@ -14,8 +17,16 @@ class ExceptionControllerAdvice {
             date = exception.date,
             message = exception.message
         )
-        return ResponseEntity
-            .status(exception.httpStatus)
-            .body(errorResponse)
+        return ResponseEntity.status(exception.httpStatus).body(errorResponse)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadable(exception: HttpMessageNotReadableException): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse(
+            httpStatus = HttpStatus.BAD_REQUEST,
+            date = LocalDateTime.now(),
+            message = exception.localizedMessage
+        )
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
     }
 }
